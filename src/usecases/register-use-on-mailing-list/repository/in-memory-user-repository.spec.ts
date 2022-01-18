179609d5ae480d335dc8a658952aff48116c57e8
Raphael Santana <raphaelstn@gmail.com>
2022-01-18 16:14:58 -0300

    test: add and find user and check its existence

--- src/usecases/register-use-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-use-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -9,4 +9,13 @@ describe('In memory user repository', () => {
     const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user).toBeNull()
   })
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const userRepo: UserRepository = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail('any@email.com')
+    expect(user?.name).toBe('any_name')
+  })
 })
