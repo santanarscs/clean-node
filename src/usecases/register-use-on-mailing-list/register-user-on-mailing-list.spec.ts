9d13d0a1088c2a33743655506133af0d6241a50f
Raphael Santana <raphaelstn@gmail.com>
2022-01-18 21:45:15 -0300

    refactor: move user data to entities layer

---
+++ src/usecases/register-use-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,17 @@
+import { UserData } from '../../entities/user-data'
+
+describe('Register User on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+
+    // const response = await useCase.registerUseOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('any@email.com')
+    // expect((await user).name).toBe('any_name')
+  })
+})
