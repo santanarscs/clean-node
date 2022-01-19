fa6413786338d156cd7364da57668572d2fd235a
Raphael Santana <raphaelstn@gmail.com>
2022-01-18 23:08:59 -0300

    feat: register valid user to maling list

---
+++ src/usecases/register-use-on-mailing-list/register-user-on-mailing-list.ts
@@ -0,0 +1,25 @@
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error'
+import { InvalidNameError } from '../../entities/errors/invalid-name-erros'
+import { User } from '../../entities/user'
+import { UserData } from '../../entities/user-data'
+import { Either, left, right } from '../../shared/either'
+import { UserRepository } from './ports/user-repository'
+
+export class RegisterUserOnMailingList {
+  private userRepo: UserRepository
+
+  constructor (userRepo: UserRepository) {
+    this.userRepo = userRepo
+  }
+
+  public async registerUseOnMailingList (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+    if (!(await this.userRepo.exists(request))) {
+      await this.userRepo.add(request)
+    }
+    return right(request)
+  }
+}
