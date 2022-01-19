5fba684532b2e8ec170ceb09afe2b00f32ded686
Raphael Santana <raphaelstn@gmail.com>
2022-01-18 22:38:45 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,4 +1,4 @@
-import { Either, left } from '../shared/either'
+import { Either, left, right } from '../shared/either'
 import { Email } from './email'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { InvalidNameError } from './errors/invalid-name-erros'
@@ -6,6 +6,15 @@ import { Name } from './name'
 import { UserData } from './user-data'
 
 export class User {
+  public readonly name: Name;
+
+  public readonly email: Email;
+
+  private constructor (name: Name, email: Email) {
+    this.name = name
+    this.email = email
+  }
+
   static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
     const emailOrError = Email.create(userData.email)
@@ -17,5 +26,9 @@ export class User {
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
   }
 }
