d08e9ac71951c5fa818a33901b78c4bc8d11b99b
Raphael Santana <raphaelstn@gmail.com>
2022-01-18 23:46:19 -0300

    chore: upgrade husky and list-staged

--- src/usecases/register-use-on-mailing-list/register-user-on-mailing-list.ts
+++ src/usecases/register-use-on-mailing-list/register-user-on-mailing-list.ts
@@ -14,9 +14,11 @@ export class RegisterUserOnMailingList {
 
   public async registerUseOnMailingList (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
     const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+
     if (userOrError.isLeft()) {
       return left(userOrError.value)
     }
+
     if (!(await this.userRepo.exists(request))) {
       await this.userRepo.add(request)
     }
