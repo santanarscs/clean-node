8292e2b05a2ca26f651be232511f5640155fbf0d
Raphael Santana <raphaelstn@gmail.com>
2022-01-24 14:56:23 -0300

    feat: handle invalid user data

--- src/usecases/send-mail/send-email.ts
+++ src/usecases/send-mail/send-email.ts
@@ -1,4 +1,6 @@
-import { Either } from '@/shared'
+import { User } from '@/entities'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { Either, left } from '@/shared'
 import { MailServiceError } from '@/usecases/errors'
 import { UseCase } from '@/usecases/ports'
 import { EmailOptions, EmailService } from '@/usecases/send-mail/ports'
@@ -12,7 +14,13 @@ export class SendEmail implements UseCase {
     this.emailService = emailService
   }
 
-  async perform (request: any): Promise<Either<MailServiceError, EmailOptions>> {
+  async perform (request: any): Promise<Either<InvalidNameError | InvalidEmailError | MailServiceError, EmailOptions>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
     const greetings = `E ai <br> ${request.name}</b>, beleza?`
     const customizeHtml = `${greetings}<br><br> ${this.emailOptions.html}`
     const emailInfo: EmailOptions = {
