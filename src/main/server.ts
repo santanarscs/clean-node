27044b687da5e0467ab5f72d67044077c7f256f4
Raphael Santana <raphaelstn@gmail.com>
2022-01-24 11:00:31 -0300

    feat: start mongodb before listening

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,9 @@
 import 'module-alias/register'
-import app from '@/main/config/app'
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-app.listen(5000, () => console.log('Server running'))
+MongoHelper.connect(process.env.MONGO_URL)
+  .then(async () => {
+    const app = (await import('@/main/config/app')).default
+    app.listen(5000, () => console.log('Server running'))
+  })
+  .catch(console.error)
