621f0d1d04c6ce4d43b3e7ab5d2db23020b0b315
Raphael Santana <raphaelstn@gmail.com>
2022-01-25 14:14:54 -0300

    refactor: return 200 instead fo 201

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,7 +1,7 @@
 import { UserData } from '@/entities'
 import { UseCase } from '@/usecases/ports'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
-import { created, badRequest, serverError } from '@/web-controllers/util'
+import { ok, badRequest, serverError } from '@/web-controllers/util'
 import { MissingParamError } from './errors/missing-param-error'
 
 export class RegisterAndSendEmailController {
@@ -27,7 +27,7 @@ export class RegisterAndSendEmailController {
         return badRequest(response.value)
       }
 
-      return created(response.value)
+      return ok(response.value)
     } catch (error) {
       return serverError(error)
     }
--- tests/web-controllers/register-user-controller.spec.ts
+++ tests/web-controllers/register-user-controller.spec.ts
@@ -62,7 +62,7 @@ describe('Register user web controller', () => {
   const errorThrowingUseCaseStub:UseCase = new ErrorThrowingUseCaseStub()
   const controller: RegisterAndSendEmailController = new RegisterAndSendEmailController(registerAndSendEmailUseCase)
 
-  test('should return status code 201 when request contains valid user data', async () => {
+  test('should return status code 200 when request contains valid user data', async () => {
     const request: HttpRequest = {
       body: {
         name: 'any_name',
@@ -71,7 +71,7 @@ describe('Register user web controller', () => {
     }
 
     const response: HttpResponse = await controller.handle(request)
-    expect(response.statusCode).toEqual(201)
+    expect(response.statusCode).toEqual(200)
     expect(response.body).toEqual(request.body)
   })
 
