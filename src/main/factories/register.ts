0a14077760d19a2ee44aa0bbd9d2fa70121c5458
Raphael Santana <raphaelstn@gmail.com>
2022-01-25 11:22:49 -0300

    refactor: correct name

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,10 +1,10 @@
-import { RegisterUserController } from '@/web-controllers'
+import { RegisterAndSendEmailController } from '@/web-controllers'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { MongoDbUserRepository } from '@/external/repositories/mongodb'
 
-export const makeRegisterUserController = (): RegisterUserController => {
+export const makeRegisterAndSendEmailController = (): RegisterAndSendEmailController => {
   const mongoDbUserRepository = new MongoDbUserRepository()
   const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mongoDbUserRepository)
-  const registerUserController = new RegisterUserController(registerUserOnMailingListUseCase)
-  return registerUserController
+  const registerAndSendEmailController = new RegisterAndSendEmailController(registerUserOnMailingListUseCase)
+  return registerAndSendEmailController
 }
