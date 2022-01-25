fc1c26c15caaaaf907a9abd2a308f43a2fa02565
Raphael Santana <raphaelstn@gmail.com>
2022-01-25 10:46:50 -0300

    feat: make use case work with users

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -1,6 +1,4 @@
-import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
 import { User, UserData } from '@/entities'
-import { Either, left, right } from '@/shared'
 import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
 import { UseCase } from '@/usecases/ports'
 
@@ -11,16 +9,13 @@ export class RegisterUserOnMailingList implements UseCase {
     this.userRepo = userRepo
   }
 
-  public async perform (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
-    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+  public async perform (request: User): Promise<UserData> {
+    const userData = { name: request.name.value, email: request.email.value }
 
-    if (userOrError.isLeft()) {
-      return left(userOrError.value)
+    if (!(await this.userRepo.exists(userData))) {
+      await this.userRepo.add(userData)
     }
 
-    if (!(await this.userRepo.exists(request))) {
-      await this.userRepo.add(request)
-    }
-    return right(request)
+    return userData
   }
 }
