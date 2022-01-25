8e040c260d5070b294db4f9359a9d391e09aaa76
Raphael Santana <raphaelstn@gmail.com>
2022-01-25 14:50:09 -0300

    test: nodemailer adapter

---
+++ tests/external/mail-services/nodemailer-email-service.spec.ts
@@ -0,0 +1,54 @@
+import { NodemailerEmailService } from '@/external/mail-services'
+import { MailServiceError } from '@/usecases/errors'
+import { EmailOptions } from '@/usecases/send-mail/ports'
+
+const attachmentFilePath = '../resources/text.txt'
+const fromName = 'Test'
+const fromEmail = 'from_email@email.com'
+const toName = 'any_email'
+const toEmail = 'any_email@email.com'
+const subject = ' Test e-mail'
+const emailBody = 'Hello world attachment test'
+const emailBodyHtml = '<b>Hello world</b>'
+const attachments = [{
+  filename: attachmentFilePath,
+  contentType: 'text/plain'
+}]
+
+const mailOptions: EmailOptions = {
+  host: 'test',
+  port: 867,
+  username: 'test',
+  password: 'test',
+  from: `${fromName} ${fromEmail}`,
+  to: `${toName} < ${toEmail} >`,
+  subject,
+  text: emailBody,
+  html: emailBodyHtml,
+  attachments
+}
+
+jest.mock('nodemailer')
+const nodemailer = require('nodemailer')
+const sendMailMock = jest.fn().mockReturnValueOnce('ok')
+nodemailer.createTransport.mockReturnValue({ sendMail: sendMailMock })
+
+describe('Nodemailer mail service adapter', () => {
+  beforeEach(() => {
+    sendMailMock.mockClear()
+    nodemailer.createTransport.mockClear()
+  })
+  test('should return ok if email is sent', async () => {
+    const nodemailer = new NodemailerEmailService()
+    const result = await nodemailer.send(mailOptions)
+    expect(result.value).toEqual(mailOptions)
+  })
+  test('should return error if email is not sent', async () => {
+    const nodemailer = new NodemailerEmailService()
+    sendMailMock.mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const result = await nodemailer.send(mailOptions)
+    expect(result.value).toBeInstanceOf(MailServiceError)
+  })
+})
